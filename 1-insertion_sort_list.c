41ed1bd6fbc8aae31299cc1be36b9834e222b080
josh.94 <jeshua.cabanillas@gmail.com>
2022-02-15 13:16:32 -0500

    style: betty style fix

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - function that sorts a doubly linked list of
+ *integers in ascending order using the Insertion sort algorithm
+ *@list: list to be sort
+ *
+ *Return: 0.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = NULL, *right = NULL, *left = NULL;
+	listint_t *tmp1 = NULL, *tmp2 = NULL;
+
+	if (!list || !(*list) || (!((*list)->prev) && !((*list)->next)))
+		return;
+
+	head = *list;
+	while (head)
+	{
+		tmp1 = head;
+		tmp2 = head->next;
+		while (tmp1 && tmp2 && tmp1->n > tmp2->n)
+		{
+			right = tmp2->next;
+			left = tmp1->prev;
+
+			tmp1->next = right;
+			if (right)
+				right->prev = tmp1;
+
+			tmp1->prev = tmp2;
+			tmp2->prev = left;
+
+			if (left)
+				left->next = tmp2;
+			else
+				*list = tmp2;
+
+			tmp2->next = tmp1;
+			tmp1 = left;
+			print_list(*list);
+		}
+		head = tmp2;
+	}
+}
