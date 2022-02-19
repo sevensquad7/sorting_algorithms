aa5f33396bbf698691475676179da727e4edec3c
josh.94 <jeshua.cabanillas@gmail.com>
2022-02-18 22:26:48 -0500

    first commit

--- sort.h
+++ sort.h
@@ -33,5 +33,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void handle(int *array, int start, int end, size_t size);
+size_t partition(int *array, int start, int end, size_t size);
 #endif /* SORT_H */
