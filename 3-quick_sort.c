1c83aea7dd1e3a6c9f55e967186976300e88dea7
josh.94 <jeshua.cabanillas@gmail.com>
2022-02-21 15:53:19 -0500

    fix: code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,85 +1,83 @@
 #include "sort.h"
 
 /**
- * partition - Partition
- * @A: Array
- * @start: start array
- * @end: End array
- * @size: Size
- *
- * Return: Partition
- */
-int partition(int *A, int start, int end, size_t size)
-{
-	int *pivot = &A[end];
-	int part_idx = start - 1;
-	int i;
-	int tmp;
-	int *ptr1 = NULL;
-	int *ptr2 = NULL;
-
-	for (i = start ; i <= end - 1 ; i++)
-	{
-		ptr1 = &A[i];
-		if (*ptr1 < *pivot)
-		{
-			part_idx++;
-			ptr2 = &A[part_idx];
-			if (*ptr2 != *ptr1)
-			{
-				tmp = *ptr1;
-				*ptr1 = *ptr2;
-				*ptr2 = tmp;
-				print_array(A, size);
-			}
-		}
-	}
-	if (A[part_idx + 1] != A[end])
-	{
-		ptr2 = &A[part_idx + 1];
-		tmp = *ptr2;
-		*ptr2  = *pivot;
-		*pivot = tmp;
-		print_array(A, size);
-
-	}
-	return (part_idx + 1);
-}
-
-/**
- * quick - Quick
- * @array: Array
- * @start: start array
- * @end: End array
- * @size: Size
+ * quick_sort - Sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ * @array: array with elements
+ * @size: size of array
  *
  * Return: Nothing
  */
-void quick(int *array, int start, int end, size_t size)
-{
-
-	int part_idx;
-
-	if (start >= end)
-		return;
-
-	part_idx = partition(array, start, end, size);
-	quick(array, start, part_idx - 1, size);
-	quick(array, part_idx + 1, end, size);
-}
-
-/**
- * quick_sort - Quick sort
- * @array: Array
- * @size: Size
- *
- * Return: Nothing
- */
-
 void quick_sort(int *array, size_t size)
 {
-	if (size <= 1)
-		return;
-	quick(array, 0, size - 1, size);
+	handle(array, 0, size, size);
 }
 
+/**
+ * handle - Creates multiple recursions to send to partition
+ * @array: array with elements
+ * @start: index of the first element
+ * @end: index of the last element
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void handle(int *array, int start, int end, size_t size)
+{
+	int index = 0;
+
+	if (end - start <= 1)
+		return;
+	index = partition(array, start, end, size);
+
+	if (index > start)
+		handle(array, start, index, size);
+
+	handle(array, index + 1, end, size);
+}
+
+/*Lomuto*/
+/**
+ * partition - Swap array elements
+ * @array: array with elements
+ * @start: index of the first element
+ * @end: index of the last element
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+size_t partition(int *array, int start, int end, size_t size)
+{
+	int i = start, j = 0, tmp = 0, index_part = 0, save = 0;
+	int pivot = array[end - 1], flag = 0;
+
+	for (i = start; i < end; i++)
+	{
+		if (array[i] > pivot)
+		{
+			for (j = i + 1; j < end; j++)
+			{
+				if (j == end - 1)
+				{
+					flag = -1;
+					save = i;
+				}
+				if (array[j] <= pivot)
+				{
+					tmp = array[j];
+					array[j] = array[i];
+					array[i] = tmp;
+					break;
+				}
+			}
+			print_array(array, size);
+			if (flag == -1)
+				break;
+		}
+	}
+	if (flag == 0)
+		return (end - 1);
+	index_part = save;
+	i = start;
+	return (index_part);
+}
